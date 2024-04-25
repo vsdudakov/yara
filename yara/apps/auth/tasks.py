091d2c1f1649436f02688d004806370d2b8dd18b
vsdudakov <vsdudakov@gmail.com>
2024-04-25 06:49:08 +0300

    Fix

--- yara/apps/auth/tasks.py
+++ yara/apps/auth/tasks.py
@@ -3,6 +3,7 @@ import typing as tp
 
 from yara.adapters.email.adapter import EmailAdapter
 from yara.core.tasks import celery_task
+from yara.main import YaraRootApp
 
 logger = logging.getLogger(__name__)
 
@@ -12,9 +13,8 @@ async def task_send_email(
     to: str,
     template_id: str,
     payload: dict[str, tp.Any],
+    root_app: YaraRootApp,
 ) -> None:
-    from yara.main import root_app
-
     email_adapter: EmailAdapter = root_app.get_adapter(EmailAdapter)
     await email_adapter.send_email(
         to,
